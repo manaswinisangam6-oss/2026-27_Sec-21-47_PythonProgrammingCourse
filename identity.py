43d39acccedc3ca412109630d221eb81e353552d
manaswinisangam6-oss <manaswinisangam6@gmail.com>
2026-09-04 14:21:54 +0530

    identity operators

---
+++ identity.py
@@ -0,0 +1,10 @@
+#  identity operators exercise 
+a=[1,2,3,4]
+b=a
+result=b is a# is identity operater
+print("result of",b,"is :",result)
+
+c=[1,2,3]
+d=[1,2,3]
+result=d is c# is identity  operator
+print("result of",d,"is",c,"is :",result) 
