57343d34ac733cb2cb5c81bd767b78cc5ffe5e41
manaswinisangam6-oss <manaswinisangam6@gmail.com>
2026-09-19 19:21:09 +0530

    demonstrate operator

---
+++ task 14.py
@@ -0,0 +1,17 @@
+# Demonstrate operator precedence and associativity
+
+# Operator precedence
+result1 = 10 + 5 * 2
+print("10 + 5 * 2 =", result1)
+
+# Parentheses have higher precedence
+result2 = (10 + 5) * 2
+print("(10 + 5) * 2 =", result2)
+
+# Associativity of arithmetic operators
+result3 = 20 - 5 - 3
+print("20 - 5 - 3 =", result3)
+
+# Right-to-left associativity of exponentiation
+result4 = 2 ** 3 ** 2
+print("2 ** 3 ** 2 =", result4)
