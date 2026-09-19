b47eed36f4c23c5659f8472312180c0769de1e92
manaswinisangam6-oss <manaswinisangam6@gmail.com>
2026-09-19 19:25:52 +0530

    demonstrate type operator

---
+++ task 15.py
@@ -0,0 +1,21 @@
+# Python program to demonstrate type conversion
+
+# String to integer
+a = "10"
+b = int(a)
+print("String to Integer:", b, type(b))
+
+# Integer to float
+c = 20
+d = float(c)
+print("Integer to Float:", d, type(d))
+
+# Float to integer
+e = 15.5
+f = int(e)
+print("Float to Integer:", f, type(f))
+
+# Integer to string
+g = 25
+h = str(g)
+print("Integer to String:", h, type(h))
