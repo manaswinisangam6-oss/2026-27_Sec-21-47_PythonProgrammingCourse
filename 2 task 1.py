fde6606704c69e49a68748899608f03d97c98bd4
manaswinisangam6-oss <manaswinisangam6@gmail.com>
2026-09-25 14:39:43 +0530

    three  angles

---
+++ 2 task 1.py
@@ -0,0 +1,10 @@
+# Check whether a triangle is valid
+
+angle1 = float(input("Enter first angle: "))
+angle2 = float(input("Enter second angle: "))
+angle3 = float(input("Enter third angle: "))
+
+if angle1 > 0 and angle2 > 0 and angle3 > 0 and angle1 + angle2 + angle3 == 180:
+    print("The triangle is valid.")
+else:
+    print("The triangle is not valid.")
